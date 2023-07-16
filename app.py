15f00538fd2a76551bef3edf4221559e781cd2be
Grigoryyy <62098460+Grigoryyy@users.noreply.github.com>
2023-07-16 19:39:28 +0300

    v4
    Выполнено требование 4
    добавлены:
        дополнительные места для грузовых машин, легковые также имеют возможность заезжать на них
        метод park_cargo, отвечающий за парковку грузовых машин
    переработаны:
        метод unpark, теперь он ведет поиск в двух словарях 
        метод park, благодаря чему легковая машина в случае отсутствия мест на парковке для легковых автомобилей ищет место на парковке для грузовых

--- app.py
+++ app.py
@@ -11,22 +11,41 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/park', methods=['POST'])
 def park():
-    car_number = request.form['car_number']
+    car_number = str(request.form['car_number'])
     hours = int(request.form['hours'])
     if car_number.lower().replace(" ", "") != '':
         return endpoint.park(car_number, hours)
     return 'Неккоректный номер машины'
 
 
+def park_cargo():
+    car_number = request.form['car_number']
+    hours = int(request.form['hours'])
+    if car_number.lower().replace(" ", "") != '':
+        return endpoint.park_cargo(car_number, hours)
+    return 'Неккоректный номер машины'
+
+
 @app.route('/unpark', methods=['POST'])
 def unpark():
-    car_number = request.form['car_number'].lower().replace(" ", "")
+    car_number = str(request.form['car_number'].lower().replace(" ", ""))
     hours = int(request.form['hours'])
     cost = endpoint.unpark(car_number, hours)
     return cost
 
 
+@app.route('/park', methods=['POST'])
+def select_park():
+    chosen_car = request.form['vehicle_type']
+    if chosen_car == 'Легковая':
+        return park()
+    elif chosen_car == 'Грузовая':
+        return park_cargo()
+    else:
+        return 'Invalid function selection'
+
+
 if __name__ == '__main__':
+
     app.run(debug=True)
--- backend.py
+++ backend.py
@@ -1,25 +1,36 @@
-global LIMIT_SEATS
-LIMIT_SEATS = 5
+LIMIT_SEATS = 2
+LIMIT_SEATS_CARGO = 2
 hourly_rate = 50
 sale_hourly_rate = 30
+
 
 class Endpoint():
 
     def __init__(self):
         self.cars = {}
+        self.cars_cargo = {}
 
     def park(self, car_number, hours):
         if len(self.cars) < LIMIT_SEATS:
             self.cars[car_number.lower().replace(" ", "")] = hours
             print(self.cars)
-            return (f"Машина {car_number} припаркована, мест осталось: {LIMIT_SEATS - int(len(self.cars))}")
+            return f"Легковая машина {car_number} припаркована на стоянке для легковых автомобилей, мест осталось: {LIMIT_SEATS - int(len(self.cars))}"
+        elif len(self.cars_cargo) < LIMIT_SEATS_CARGO and len(self.cars) >= LIMIT_SEATS:
+            self.cars_cargo[car_number.lower().replace(" ", "")] = hours
+            return f"Легковая машина {car_number} припаркована на стоянке для грузовых автомобилей, мест осталось: {LIMIT_SEATS_CARGO - int(len(self.cars_cargo))}"
         else:
-            return "Достигнуто максимальное количество машин. Парковка больше недоступна."
+            return "Достигнуто максимальное количество машин. Парковки для грузовых и легковых автомобилей больше недоступны."
+
+    def park_cargo(self, car_number, hours):
+        if len(self.cars_cargo) < LIMIT_SEATS_CARGO:
+            self.cars_cargo[car_number.lower().replace(" ", "")] = hours
+            print(self.cars_cargo)
+            return f"Грузовая машина {car_number} припаркована на стоянке для грузовых автомобилей, мест осталось: {LIMIT_SEATS_CARGO - int(len(self.cars_cargo))}"
+        else:
+            return "Достигнуто максимальное количество машин. Парковка для грузовых автомобилей больше недоступна."
 
     def unpark(self, car_number, hours):
-        if car_number not in self.cars:
-            return(f"Машины с номером {car_number} нет на парковке!")
-        else:
+        if car_number in self.cars:
             current_hour = self.cars[car_number]
             cost = hours // 24 * 1020
             hours %= 24
@@ -31,4 +42,18 @@ class Endpoint():
                 current_hour += 1
                 current_hour %= 24
             del self.cars[car_number]
-            return(f"Машина с номером {car_number} уехала! стоимость парковки: {cost}")
+            return f"Машина с номером {car_number} уехала! стоимость парковки: {cost}"
+        elif car_number in self.cars_cargo:
+            current_hour = self.cars_cargo[car_number]
+            cost = hours // 24 * 1020
+            hours %= 24
+            for i in range(hours):
+                if current_hour >= 7 and current_hour < 22:
+                    cost += hourly_rate
+                else:
+                    cost += sale_hourly_rate
+                current_hour += 1
+                current_hour %= 24
+            del self.cars_cargo[car_number]
+            return f"Машина с номером {car_number} уехала! стоимость парковки: {cost}"
+        else: return f"Машины с номером {car_number} нет на парковках!"
